87a91405fd8444e3b2b0762c037daf463d35b4f9
Félix Larose-Gervais <filedesless@gmail.com>
2022-12-06 08:18:13 -0500

    repeated calls to append() > insert()

--- 2022/day05/sol.py
+++ 2022/day05/sol.py
@@ -12,7 +12,7 @@ a = [
     ['B', 'D', 'Z'],
     ['M', 'N', 'Z', 'W']
 ]
-b = [stack.copy() for stack in a]
+b = [stack[:] for stack in a]
 
 
 path = os.path.dirname(__file__)
@@ -20,10 +20,10 @@ lines = [line.strip() for line in open(path + '/input.txt')]
 
 for line in lines:
     n, s, d = [int(i) for i in line.split(' ') if i.isnumeric()]
-    p = len(b[d-1])
-    for _ in range(n):
+    for i in b[s-1][-n:]:
         a[d-1].append(a[s-1].pop())
-        b[d-1].insert(p, b[s-1].pop())
+        b[d-1].append(i)
+    b[s-1] = b[s-1][:-n]
 
 print("".join([stack[-1] for stack in a]))
 print("".join([stack[-1] for stack in b]))
