8468910d61716293e8c52c2ee741413c7ea12cf1
Félix Larose-Gervais <filedesless@gmail.com>
2024-01-04 13:17:07 -0500

    day08 part 02

--- 2023/08/sol.py
+++ 2023/08/sol.py
@@ -1,19 +1,23 @@
 from itertools import cycle
+from math import lcm
 
 
 lines = [line.strip() for line in open("input", encoding="utf8")]
 
-graph = dict()
+graph: dict[str, (str, str)] = {}
 for line in lines[2:]:
-    words = ["".join(filter(str.isalpha, word)) for word in line.split()]
+    words = ["".join(filter(str.isalnum, word)) for word in line.split()]
     graph[words[0]] = (words[2], words[3])
-print(graph)
 
-current, i = 'AAA', 0
-for direction in cycle(lines[0]):
-    current = graph[current][0 if direction == "L" else 1]
-    i += 1
-    if current == 'ZZZ':
-        break
+currents = [node for node in graph if node.endswith('A')]
+j = currents.index('AAA')
+steps = [0 for _ in currents]
+for (i, _) in enumerate(currents):
+    for direction in cycle(lines[0]):
+        currents[i] = graph[currents[i]][0 if direction == "L" else 1]
+        steps[i] += 1
+        if currents[i].endswith('Z'):
+            break
 
-print(i)
+print('part 01:', steps[j])
+print('part 02:', lcm(*steps))
